d5b5bf84ef674c9767f6a61c5c10fd10d55d6115
Michael Lan <al1714241@gmail.com>
2023-10-12 21:56:51 +0800

    v2.2
    回到tkinter
    重新寫程式

--- main.py
+++ main.py
@@ -1,110 +1,96 @@
-import customtkinter as ctk
-from CTkMessagebox import CTkMessagebox
-import subprocess, ctypes, sys, os, win32api
+import subprocess
+import ctypes
+import sys
+import win32api
+import tkinter as tk
+from tkinter import messagebox, ttk
 
-class App(ctk.CTk):
+class WarframePairBlockTool(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.geometry("250x325")
-        self.title("WarframePairBlockTool")
-        self.my_font = ctk.CTkFont(family="Microsoft JhengHei", size=14) 
+        self.title('WarframePairBlockTool')
+        self.geometry('250x325')
+        self.create_widgets()
+        self.resizable(0, 0)
 
-        self.headerLabel = ctk.CTkLabel(self, text="Warframe 配對限制器(強制主機)\n[封鎖UDP輸出埠]", font=self.my_font)
-        self.headerLabel.grid(row=0, column=0, padx=25, pady=0)
+    def create_widgets(self):
+        header_label = tk.Label(self, text='Warframe 配對限制器（強制主機）\n[ 封鎖 UDP 輸出 Port ]')
+        header_label.pack()
 
-        self.statusLabel = ctk.CTkLabel(self ,font=ctk.CTkFont(family="Microsoft JhengHei", size=18), anchor="center")
-        self.statusLabel.grid(row=1, column=0, pady=5, sticky="nsew")
+        self.status_label = tk.Label(self, font="Arial,12")
+        self.status_label.pack()
 
-        self.tipLabel = ctk.CTkLabel(master=self,text="選擇您 Warframe 的 UDP 埠",font=self.my_font, anchor="center")
-        self.tipLabel.grid(row=2, column=0, padx=25, pady=1)
+        tip_label = tk.Label(self, text="選擇您Warframe內的UDP埠")
+        tip_label.pack()
 
-        self.portComboBox = ctk.CTkComboBox(
-            self,
+        self.port_combo = ttk.Combobox(self,
             values=[
-                "4950 & 4955", 
+                "4950 & 4955",
                 "4960 & 4965",
                 "4970 & 4975",
                 "4980 & 4985",
                 "4990 & 4995",
-                "3074 & 3080"
-            ],
-            height=30,
-            width=200,
-            corner_radius=10)
-        self.portComboBox.grid(row=3, column=0, padx=10, pady=1)
+                "3074 & 3080"])
+        self.port_combo.pack()
+        self.port_combo.current(0)
+        hint_label = tk.Label(self, text="（若是預設可以不用更動）\n")
+        hint_label.pack()
 
-        self.checkBtn = ctk.CTkButton(
-            self,
-            text='查看防火牆',
-            height = 35,
-            width = 200,
-            command=self.checkFireWall,
-            font=ctk.CTkFont(family="Microsoft JhengHei")
-        )
-        self.checkBtn.grid(row=4, column=0, pady=3)
+        btn_frame = tk.Frame(self)
+        btn_frame.pack(side=tk.TOP)
+        btn_check = tk.Button(btn_frame, text='查看防火牆', bg="gray90", height=1, width=30, command=self.check_rule)
+        btn_check.grid(row=0, column=0)
+        btn_generate = tk.Button(btn_frame, text='阻止配對[封鎖UDP輸出]', bg="firebrick2", fg="white", height=3, width=30, command=self.create_rule)
+        btn_generate.grid(row=1, column=0)
+        btn_del = tk.Button(btn_frame, text='恢復配對[恢復UDP輸出]', bg="SpringGreen3", fg="black", height=3, width=30, command=self.del_rule)
+        btn_del.grid(row=2, column=0)
 
-        self.blockBtn = ctk.CTkButton(
-            self, 
-            text='阻止配對[封鎖UDP輸出]', 
-            fg_color="firebrick2", 
-            text_color="white", 
-            height = 50,
-            width = 200,
-            command=self.createRule,
-            font=ctk.CTkFont(family="Microsoft JhengHei") 
-        )
-        self.blockBtn.grid(row=5, column=0, pady=1)
+        author_label = tk.Label(self, text='\n此工具由小翔製作\n Discord: XiaoXiang_Meow#6647')
+        author_label.pack()
+        self.rule_status()  # 開啟時偵測規則是否存在
 
-        self.restoreBtn = ctk.CTkButton(
-            self, 
-            text='恢復配對[恢復UDP輸出]', 
-            fg_color="SpringGreen3", 
-            text_color="black", 
-            height = 50,
-            width = 200,
-            command=self.deleteRule,
-            font=ctk.CTkFont(family="Microsoft JhengHei") 
-        )
-        self.restoreBtn.grid(row=6, column=0, pady=1)
-
-        self.footerLabel = ctk.CTkLabel(master=self,text="此工具由小翔製作\nDiscord: XiaoXiang_Meow#6647",font=self.my_font, anchor="center") 
-        self.footerLabel.grid(row=7, column=0, pady=2, sticky="nsew")
-        
-    def ruleStatus(self):
+    def rule_status(self):
         status = subprocess.run('netsh advfirewall firewall show rule name=WarframePairBlockPort dir=out', shell=True)
         if status.returncode == 1:
-            self.statusLabel.configure(text="目前狀態：配對目前正常", text_color="black", fg_color= "SpringGreen3")
+            self.status_label['bg'] = 'SpringGreen3'
+            self.status_label['fg'] = 'Black'
+            self.status_label['text'] = '目前狀態：配對目前正常'
         elif status.returncode == 0:
-            self.statusLabel.configure(text="目前狀態：配對已阻斷", text_color="white", fg_color= "firebrick2")
+            self.status_label['bg'] = 'firebrick2'
+            self.status_label['fg'] = 'white'
+            self.status_label['text'] = '目前狀態：配對已阻斷'
 
-    def createRule(self):
-        WFPORT = self.portComboBox.get().split(' & ')
+    def create_rule(self):
+        WFPORT = self.port_combo.get().split(' & ')
         status = subprocess.run(F"netsh advfirewall firewall add rule name=WarframePairBlockPort protocol=UDP dir=out localport={WFPORT[0]}-{WFPORT[1]} action=block", shell=True)
         if status.returncode == 0:
             subprocess.run("netsh advfirewall firewall set rule name=WarframePairBlockPort new enable=yes", shell=True)
         elif status.returncode == 1:
-            CTkMessagebox(title="警告", message="目前配對已阻斷\n請勿重複點按", icon="warning", option_1="關閉", font=self.my_font)
-        self.ruleStatus()
-        
-    def deleteRule(self):
+            messagebox.showinfo('訊息', '請用系統管理員身分執行程式')
+        self.rule_status()
+
+    def del_rule(self):
         status = subprocess.run("netsh advfirewall firewall delete rule name=WarframePairBlockPort", shell=True)
         if status.returncode == 0:
-            self.ruleStatus()
+            self.rule_status()
         elif status.returncode == 1:
-            CTkMessagebox(title="警告", message="目前配對狀態為正常\n請勿重複點按", icon="warning", option_1="關閉", font=self.my_font)
-            self.ruleStatus()
+            messagebox.showinfo('訊息', '請用系統管理員身分執行程式\n或目前配對狀態為正常')
+            self.rule_status()
 
-    def checkFireWall(self):
+    def check_rule(self):
         win32api.ShellExecute(0, 'open', 'wf.msc', '', '', 1)
-        self.ruleStatus()
+        self.rule_status()
 
 if __name__ == "__main__":
-    try:
-        if ctypes.windll.shell32.IsUserAnAdmin():
-            app = App()
-            app.ruleStatus()
-            app.mainloop()
-        else:
-            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, os.path.abspath(sys.argv[0]), None, 1)
-    except:
-        pass
+    def is_admin():
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+
+    if is_admin():
+        app = WarframePairBlockTool()
+        app.mainloop()
+    else:
+        if sys.version_info[0] == 3:
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, sys.argv[0], None, 1)
